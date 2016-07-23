40eb8a1742c218afa08c90c7fdf9d2083b4c96ba
radiationx <radiationx@iMac-radiationx.local>
2016-07-23 16:47:24 +0300

    fixes

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -2,5 +2,4 @@ var drawer;
 window.addEventListener("load", function (e) {
     drawer = new Drawer(document.getElementById("drawer"), document.getElementById("drawer_bg"));
     drawer.setDrawerIcon(new DrawerIcon(document.getElementById("rx_icon")));
-    drawer.setWrapper(document.getElementById("wrapper"));
 });
--- demo/scripts/rx_icon.js
+++ demo/scripts/rx_icon.js
@@ -29,22 +29,22 @@ function DrawerIcon(icon) {
         transX2 = -0.01 * percent;
         if (direction) rotateIc = 1.80 * percent;
         else rotateIc = 360 - (1.80 * percent);
-        setProperty(line1, transformProp, "rotate(" + rotateLine + "deg) scaleX(" + scaleX + ") translateY(" + transY + "px) translateX(" + transX + "px)");
-        setProperty(line2, transformProp, "scaleX(" + scaleX2 + ") translateX(" + transX2 + "px)");
-        setProperty(line3, transformProp, "rotate(" + (-rotateLine) + "deg) scaleX(" + scaleX + ") translateY(" + (-transY) + "px) translateX(" + transX + "px)");
-        setProperty(ic, transformProp, "rotate(" + rotateIc + "deg)");
+        setProperty(line1, transformProp, "rotate(" + rotateLine + "deg) scaleX(" + scaleX + ") translateY(" + transY + "px) translateX(" + transX + "px)" + trZ);
+        setProperty(line2, transformProp, "scaleX(" + scaleX2 + ") translateX(" + transX2 + "px)" + trZ);
+        setProperty(line3, transformProp, "rotate(" + (-rotateLine) + "deg) scaleX(" + scaleX + ") translateY(" + (-transY) + "px) translateX(" + transX + "px)" + trZ);
+        setProperty(ic, transformProp, "rotate(" + rotateIc + "deg)" + trZ);
     }
     this.setState = function (state, duration) {
         duration = duration || 0.225;
         enableAnimation(duration);
         var temp = this;
         switch (state) {
-        case 0:
-            this.set(1);
-            break;
-        case 1:
-            this.set(100);
-            break;
+            case 0:
+                this.set(1);
+                break;
+            case 1:
+                this.set(100);
+                break;
         }
         setTimeout(function () {
             disableAnimation();
@@ -53,7 +53,7 @@ function DrawerIcon(icon) {
     }
 
     function enableAnimation(duration) {
-        var transition = "all " + duration + "s ease";
+        var transition = propPrefixCss + "transform " + duration + "s ease";
         setProperty(line1, transitionProp, transition);
         setProperty(line2, transitionProp, transition);
         setProperty(line3, transitionProp, transition);
@@ -76,8 +76,10 @@ function DrawerIcon(icon) {
     function setProperty(elem, property, value) {
         elem.style[property] = value;
     }
-    var transformProp = "transform"
-        , transitionProp = "transition";
+    var transformProp = "transform",
+        transitionProp = "transition",
+        trZ = "translateZ(0)", propPrefixCss = "";
+
     (function () {
         icon.innerHTML += '<span class="ic"><i class="line one"></i><i class="line two"></i><i class="line thr"></i></span>';
         ic = icon.querySelector(".ic");
@@ -85,17 +87,22 @@ function DrawerIcon(icon) {
         line2 = ic.querySelector(".two");
         line3 = ic.querySelector(".thr");
         //Find prop name
-        var testEl = document.createElement('div');
-        if (testEl.style.transform == null) {
-            var vendors = ['Webkit', 'Moz', 'ms'];
+        var testEl = document.createElement('div'),
+            vendors;
+        if (testEl.style.transform === undefined) {
+            vendors = ['Webkit', 'Moz', 'ms', 'O'];
             for (var vendor in vendors) {
                 if (testEl.style[vendors[vendor] + 'Transform'] !== undefined) {
                     transformProp = vendors[vendor] + 'Transform';
+                    propPrefixCss = "-" + vendors[vendor].toLowerCase() + "-";
                 }
                 if (testEl.style[vendors[vendor] + 'Transition'] !== undefined) {
                     transitionProp = vendors[vendor] + 'Transition';
                 }
             }
+        }
+        if (/.*opera.*presto/i.test(navigator.userAgent)) {
+            trZ = "";
         }
     })();
 }
