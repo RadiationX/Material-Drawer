6c571b80320ec467e95b58e06aced801e8187a7a
radiationx <radiationx@iMac-radiationx.local>
2016-07-24 23:25:01 +0300

    update

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -6,4 +6,22 @@ window.addEventListener("load", function (e) {
     iconElem = document.getElementById("rx_icon");
     drawer = new Drawer(drawerElem);
     drawer.setDrawerIcon(new DrawerIcon(iconElem));
+    drawer.onOpenListener(function () {
+        console.log("open");
+    });
+    drawer.onCloseListener(function () {
+        console.log("close");
+    });
+    drawer.onMoveListener(function (x, percent, animation, duration) {
+        console.log(x + " " + percent + " " + animation, duration);
+    });
+    setTimeout(function () {
+        drawer.openDrawer();
+    }, 1000);
+    setTimeout(function () {
+        drawer.closeDrawer();
+    }, 2000);
+    setTimeout(function () {
+        drawer.toggleDrawer();
+    }, 3000);
 });
